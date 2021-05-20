4cc92697618c3252969c36d262e5ed7bfe6bfc1a
AYUSTARK <ayushrj65@gmail.com>
2021-05-20 22:58:22 +0530

    Update orders.js

--- Api/routes/orders.js
+++ Api/routes/orders.js
@@ -2,7 +2,29 @@ const express = require("express")
 const router = express.Router()
 
 router.get("",(req, res) => {
+    res.status(200).json({
+        orders: "Orders mila ya nhii?? Maine to bhej diya🤔"
+    })
+})
 
+router.post("",(req, res) => {
+    res.status(200).json({
+        orders: "Order Created"
+    })
+})
+
+router.get("/:orderId",(req, res) => {
+    res.status(200).json({
+        get: "Order Details queried",
+        id: req.params.orderId
+    })
+})
+
+router.delete("/:orderId",(req, res) => {
+    res.status(200).json({
+        get: "Order Deleted",
+        id: req.params.orderId
+    })
 })
 
 module.exports = router
