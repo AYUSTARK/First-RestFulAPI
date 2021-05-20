dc2f18e8b650bf292eeff023e46f17952b9ec66d
AYUSTARK <ayushrj65@gmail.com>
2021-05-20 21:31:44 +0530

    Update products.js

--- Api/routes/products.js
+++ Api/routes/products.js
@@ -1,13 +1,29 @@
 const express = require("express")
 const router = express.Router()
-router.get("/",((req, res, next) =>{
-    res.json({
+router.get("",(req, res, next) =>{
+    res.status(200).json({
         "message": "Handling Get Requests to /products"
     })
-}))
+})
 
-router.post("/",((req, res, next) =>{
-    res.json({
+router.post("",(req, res, next) =>{
+    res.status(250).json({
         "message": "Handling Post Requests to /products"
     })
-}))
+})
+
+router.get("/:productId",(req, res, next) => {
+    const pId = req.params.productId;
+    if (pId === "special"){
+        res.status(200).json({
+            msg: "Congrats for getting "+pId
+        })
+    }else {
+        res.status(400).json({
+            message: "It's not special...Boooooo",
+            id: pId
+        })
+    }
+})
+
+module.exports = router
