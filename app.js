ff21899828e02064aa4e5e0bf11d0c81c530bedb
AYUSTARK <ayushrj65@gmail.com>
2021-06-05 21:06:46 +0530

    update

--- app.js
+++ app.js
@@ -3,12 +3,31 @@ const app = express()
 const mor = require("morgan")
 const bParser = require("body-parser")
 const mongoose = require("mongoose")
+const mySql = require("mysql")
 
 //Routes to handle requests
 const productsRoute = require("./Api/routes/products");
 const ordersRoute = require("./Api/routes/orders");
 const userRoute = require("./Api/routes/user");
 const pass = "apistorm"
+
+/*const con = mySql.createConnection({
+    host: "162.214.80.64",
+    user: "nishanat_api",
+    password: "jayant@1986",
+    database: "nishanat_users"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+const sql = "Create table user(name Varchar(25));"
+con.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log("Result: " + result);
+})*/
+
 mongoose.connect("mongodb+srv://Storm:"+process.env.mongoPass+"@mongo.yjmso.mongodb.net/firstDB?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
